a59c72c5c0a69f5f2f8149ef6a9e3daee549fddb
K Arvind Reddy <amtkumbham@gmail.com>
2013-09-05 10:59:13 +0530

    Initial check in

---
+++ sample1/src/simple/simple1.java
@@ -0,0 +1,13 @@
+package simple;
+
+public class simple1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hellow world!");
+	}
+
+}
